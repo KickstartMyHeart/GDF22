b32d6ec4ed22101b6effebb2280ee2e784cb3335
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2012-10-27 17:56:18 -0400

    Added new JS file for playership
    what's love got to do with it?

--- gdf22.js
+++ gdf22.js
@@ -3,12 +3,17 @@ function GDF22() {
 	this.stageOptions = {
 		height: 600,
 		width: 800
-	}
+	};
 
 	this.testimg = new $.gameQuery.Animation({imageURL: "images/trollface.png"});
+	
+	this.scroll = function () {
+		
+	};
 
 	$("#stage").playground(this.stageOptions)
-		.addSprite("testimg", {animation: this.testimg, width: 800, height: 600});
+		.addGroup("bgLayer")
+			.addSprite("testimg", {animation: this.testimg, width: 800, height: 600});
 		
 	$.playground().startGame(function () {
 		alert("FUCK YOU");
