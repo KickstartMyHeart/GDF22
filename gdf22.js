207f70f1ff6e3657f9e99ce50ca7b9387e7ccae3
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2012-10-27 21:03:13 -0400

    Added functionality for background scrolling, still incomplete.
    Background scrolls now but tiling does not work quite the way it should.

--- gdf22.js
+++ gdf22.js
@@ -1,19 +1,50 @@
-function GDF22() {
+function GDF22() {	
+
+	var that = this;
 
 	this.stageOptions = {
 		height: 600,
 		width: 800
 	};
 
-	this.testimg = new $.gameQuery.Animation({imageURL: "images/trollface.png"});
+	this.testimg = new $.gameQuery.Animation({imageURL: "images/star.jpg"});
+	this.testimg.width = 563;
+	this.testimg.height = 422;
 	
 	this.scroll = function () {
 		
 	};
 
 	$("#stage").playground(this.stageOptions)
-		.addGroup("bgLayer")
-			.addSprite("testimg", {animation: this.testimg, width: 800, height: 600});
+		.addGroup("bgLayer");
+	
+	var xLimit = Math.ceil(this.stageOptions.width / this.testimg.width);
+	var yLimit = Math.ceil(this.stageOptions.height / this.testimg.height);
+	
+	for (var i = 0; i < xLimit; i++) {
+		for (var j = 0; j < yLimit; j++) {
+			$("#bgLayer").addSprite("testimg" + i.toString() + j.toString(), {
+				animation: this.testimg,
+				width: this.testimg.width,
+				height: this.testimg.height,
+				posx: this.testimg.width * i,
+				posy: this.testimg.height * j
+			});
+		}
+	}
+	
+	console.log($("#bgLayer").children());
+	
+	$.playground().registerCallback(function () {
+		$("#bgLayer").children().each(function () {
+			if ($(this).y() >= that.stageOptions.height) {
+				$(this).y($(this).h() * -2, true);
+				// $(this).y($(this).h() * -1);
+			} else {
+				$(this).y(5, true);
+			}
+		});
+	}, 30);
 		
 	$.playground().startGame(function () {
 		alert("FUCK YOU");
---
+++ level.js
@@ -0,0 +1,12 @@
+function Level(background) {
+	this.background = background;
+}	
+
+Level.prototype.create = function () {
+	// Initialize all of the shit that has to go into making a level.
+	// Is this necessary?
+};
+
+Level.prototype.scrollBG = function () {
+	// Scroll the background downward at a specific duration.
+};
