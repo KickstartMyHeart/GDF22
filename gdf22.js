df0876b2f205d5fa0a16184ce051234028940c12
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2012-11-02 20:28:19 -0400

    Fixed the scrolling for the background.
    Made the drawing loops start at -2 instead of 0 for image prepopulation.
    Addee scrollspeed variable to fix the tearing issue.

--- gdf22.js
+++ gdf22.js
@@ -14,6 +14,8 @@ function GDF22() {
 	this.scroll = function () {
 		
 	};
+	
+	$("#stage").css('background-color', 'black');
 
 	$("#stage").playground(this.stageOptions)
 		.addGroup("bgLayer");
@@ -21,8 +23,8 @@ function GDF22() {
 	var xLimit = Math.ceil(this.stageOptions.width / this.testimg.width);
 	var yLimit = Math.ceil(this.stageOptions.height / this.testimg.height);
 	
-	for (var i = 0; i < xLimit; i++) {
-		for (var j = 0; j < yLimit; j++) {
+	for (var i = -1; i < xLimit; i++) {
+		for (var j = -1; j < yLimit; j++) {
 			$("#bgLayer").addSprite("testimg" + i.toString() + j.toString(), {
 				animation: this.testimg,
 				width: this.testimg.width,
@@ -36,12 +38,14 @@ function GDF22() {
 	console.log($("#bgLayer").children());
 	
 	$.playground().registerCallback(function () {
+		var scrollspeed = 3;
+	
 		$("#bgLayer").children().each(function () {
 			if ($(this).y() >= that.stageOptions.height) {
-				$(this).y($(this).h() * -2, true);
+				$(this).y(Math.floor($(this).h() * -3) + scrollspeed, true);
 				// $(this).y($(this).h() * -1);
 			} else {
-				$(this).y(5, true);
+				$(this).y(scrollspeed, true);
 			}
 		});
 	}, 30);
